c0bc452166900ab09959c4b6a561a27e6eba080f
fatnat <natalie.zhang01@sap.com>
2017-01-23 16:00:43 +0800

    Minify HTML file

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var gulp = require('gulp'),
     connect = require('gulp-connect'),
     gulpif = require('gulp-if'),
     uglify = require('gulp-uglify'),
+    minifyHTML = require('gulp-minify-html'),
     concat = require('gulp-concat');
 
 // add environment variables
@@ -87,7 +88,7 @@ gulp.task('watch', function() {
     gulp.watch(coffeeSources, ['coffee']);
     gulp.watch(jsSources, ['js']);
     gulp.watch('components/sass/*.scss', ['compass']);
-    gulp.watch(htmlSources, ['html']);
+    gulp.watch('builds/development/*.html', ['html']);
     gulp.watch(jsonSources, ['json']);
 });
 
@@ -102,7 +103,13 @@ gulp.task('connect', function() {
 
 // set upt live reload for static files, eg. HTML
 gulp.task('html', function() {
-    gulp.src(htmlSources)
+    gulp.src('builds/development/*.html')
+        .pipe(
+            gulpif(env === 'production', minifyHTML())
+        )
+        .pipe(
+            gulpif(env === 'production', gulp.dest(outputDir))
+        )
         .pipe(
             connect.reload()
         )
