16839c224c989f1eb4f1b06a9115cafe67a38dcb
fatnat <natalie.zhang01@sap.com>
2017-01-23 15:19:36 +0800

    Add html and json watch

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require('gulp'),
     coffee = require('gulp-coffee'),
     browserify = require('gulp-browserify'),
     compass = require('gulp-compass'),
+    connect = require('gulp-connect'),
     concat = require('gulp-concat');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
@@ -11,7 +12,10 @@ var jsSources = ['components/scripts/tagline.js',
 			    'components/scripts/pixgrid.js', 
 			    'components/scripts/template.js'];
 var sassSources = ['components/sass/style.scss'];
+var htmlSources = ['builds/development/*.html'];
+var jsonSources = ['builds/development/js/*.json'];
 
+// process coffee script
 gulp.task('coffee', function() {
     gulp.src(coffeeSources)
         .pipe(
@@ -23,6 +27,7 @@ gulp.task('coffee', function() {
         .pipe(gulp.dest('components/scripts'));
 });
 
+// process js scripts
 gulp.task('js', function() {
 	gulp.src(jsSources)
 		.pipe(
@@ -33,9 +38,13 @@ gulp.task('js', function() {
         )
 		.pipe(
 			gulp.dest('builds/development/js/')
-		);
+		)
+        .pipe(
+            connect.reload()
+        );
 });
 
+// process sass files
 gulp.task('compass',function() {
     gulp.src(sassSources)
         .pipe(
@@ -46,13 +55,46 @@ gulp.task('compass',function() {
             })
             .on('error', gutil.log)
         )
-        .pipe(gulp.dest('builds/development/css/'))
+        .pipe(
+            gulp.dest('builds/development/css/')
+        )
+        .pipe(
+            connect.reload()
+        );
 });
 
+// watch any dynamic file changes
 gulp.task('watch', function() {
     gulp.watch(coffeeSources, ['coffee']);
     gulp.watch(jsSources, ['js']);
     gulp.watch('components/sass/*.scss', ['compass']);
+    gulp.watch(htmlSources, ['html']);
+    gulp.watch(jsonSources, ['json']);
 });
 
-gulp.task('default', ['coffee', 'js', 'compass', 'watch']);
+
+// set up browser live reload
+gulp.task('connect', function() {
+    connect.server({
+        root: 'builds/development/',
+        livereload: true
+    });
+});
+
+// set upt live reload for static files, eg. HTML
+gulp.task('html', function() {
+    gulp.src(htmlSources)
+        .pipe(
+            connect.reload()
+        )
+});
+
+gulp.task('json', function() {
+    gulp.src(jsonSources)
+        .pipe(
+            connect.reload()
+        )
+});
+
+// include all tasks in the default task
+gulp.task('default', ['coffee', 'js', 'compass', 'html', 'json', 'connect', 'watch']);
